cc0e53f82c934e326f132534d3abc8f625373deb
fkoh111 <frederik_kok@icloud.com>
2020-03-15 14:27:27 +0100

    Added try catch

--- src/components/Wttr.jsx
+++ src/components/Wttr.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 
 export default class Wttr extends React.Component {
-  state = {
-    fetching: true,
-    ip: null
-  };
-
-  async componentDidMount() {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const url = "http://wttr.in/Denmark?format=3&?m";
-    const response = await fetch(proxy + url);
-    const data = await response.text();
-    this.setState({ ip: data, fetching: false });
+  constructor() {
+    super();
+    this.state = {
+      fetching: true,
+      ip: null
+    };
   }
 
+  async componentDidMount() {
+    try {
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const url = "http://wttr.in/Denmark?format=3&?m";
+      const response = await fetch(proxy + url);
+      const result = await response.text();
+      this.setState({ ip: result, fetching: false });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   render() {
     return (
       <div>
         {this.state.fetching || !this.state.ip ? (
-          <div>fetching...</div>
+          <div>...</div>
         ) : (
           <div>{this.state.ip}</div>
         )}
