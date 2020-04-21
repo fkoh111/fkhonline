9fef6aba5db6b2915261f53fa1ca4d0aac72aa8a
fkoh111 <frederik_kok@icloud.com>
2020-04-21 06:52:24 +0200

    Removed seconds attribute

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Splash extends Component {
   constructor(props) {
     super(props);
-    this.state = { seconds: 0, twistedHeader: false };
+    this.state = { twistedHeader: false };
   }
 
   twist() {
