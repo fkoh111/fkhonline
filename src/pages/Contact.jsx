16f6a568d6ffe189665e3f1575181e159387c277
fkoh111 <frederik_kok@icloud.com>
2020-04-22 18:25:23 +0200

    Pruned contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
     <div className="textStyle">
       <h2>Contact</h2>
+      <ContactForm />
     </div>
   );
 };
