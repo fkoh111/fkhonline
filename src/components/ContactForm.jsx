361da6596b0e5fffe2203caf42598e073e995b59
fkoh111 <frederik_kok@icloud.com>
2020-04-24 01:37:27 +0200

    Added contact schema validation with Yup

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
 
 /**
  * On the pattern, see:
@@ -8,38 +9,59 @@ import { Formik, Field, Form } from "formik";
  *     - https://github.com/jaredpalmer/formik/issues/1420
  */
 
+const ContactSchema = Yup.object().shape({
+  name: Yup.string().min(2, "Too Short!").max(50, "Too Long!"),
+  email: Yup.string().email("Invalid email"),
+  message: Yup.string()
+    .min(20, "Message too short!")
+    .max(500, "Message too long!"),
+});
+
 const ContactForm = () => {
   return (
     <Formik
       initialValues={{ name: "", email: "", message: "" }}
+      validationSchema={ContactSchema}
       onSubmit={(values, { resetForm }) => {
         const payload = JSON.stringify(values, null, 2);
         console.log(payload);
         resetForm({ values: "" });
       }}
     >
-      <Form>
-        <div>
-          <label htmlFor="name">Name: </label> <div></div>
-          <Field name="name" type="text" placeholder="Name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email: </label> <div></div>
-          <Field name="email" type="email" placeholder="Email" />
-        </div>
-        <p></p>
-        <div>
-          <Field
-            name="message"
-            component="textarea"
-            placeholder="Message..."
-            rows={10}
-          />
-        </div>
-        <button className="buttonStyle" type="submit">
-          Send
-        </button>
-      </Form>
+      {({ errors, touched }) => (
+        <Form>
+          <div>
+            <label htmlFor="name">Name: </label> <div></div>
+            <Field name="name" type="text" placeholder="Name" />
+            {errors.name && touched.name ? (
+              <div className="errStyle">{errors.name}</div>
+            ) : null}
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label> <div></div>
+            <Field name="email" type="email" placeholder="Email" />
+            {errors.email && touched.email ? (
+              <div className="errStyle">{errors.email}</div>
+            ) : null}
+          </div>
+          <p></p>
+          <div>
+            <Field
+              name="message"
+              component="textarea"
+              placeholder="Message..."
+              rows={10}
+            />
+            <div></div>
+            {errors.message && touched.message ? (
+              <div className="errStyle">{errors.message}</div>
+            ) : null}
+          </div>
+          <button className="buttonStyle" type="submit">
+            Send
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
