d4e4cbc9bc63b72ad04c89d44ec37dd6dcedbe11
fkoh111 <frederik_kok@icloud.com>
2020-04-24 02:26:58 +0200

    Correct errmsgs

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,7 +10,7 @@ import * as Yup from "yup";
  */
 
 const ContactSchema = Yup.object().shape({
-  name: Yup.string().min(2, "Too Short!").max(50, "Too Long!"),
+  name: Yup.string().min(2, "Too short!").max(50, "Too long!"),
   email: Yup.string().email("Invalid email!"),
   message: Yup.string()
     .min(20, "Message too short!")
