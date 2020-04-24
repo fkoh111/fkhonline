6b98561072e935cc2da5c187e639116ac7d34ec1
fkoh111 <frederik_kok@icloud.com>
2020-04-24 10:58:36 +0200

    Contact form styling for mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,6 +5,12 @@ const Contact = () => {
   return (
     <div className="textStyle">
       <h2>Contact</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed
+        placerat erat, a pellentesque metus. Phasellus purus nisl, suscipit at
+        scelerisque at, faucibus a nibh. Aenean et finibus nunc, ut ultricies
+        velit. Nunc finibus vel est ac semper.
+      </p>
       <div className="contactLayout">
         <div>
           <ContactForm />
