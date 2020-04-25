cc646ce03e2876c3214cb8f2ceb880cfa4808b11
fkoh111 <frederik_kok@icloud.com>
2020-04-25 22:09:36 +0200

    Added simple posting to local hosted endpoint

--- src/helpers/utils.jsx
+++ src/helpers/utils.jsx
@@ -2,12 +2,27 @@
  * Write some commments for the function
  *
  * Potentially add endpoint as param as well.
+ *
+ * Catch errors and all that jazz. Pass proper messages from the backend
  */
 
-const postHandler = (data) => {
+const postHandler = async (data) => {
   data.timestamp = Date.now();
   const payload = JSON.stringify(data);
-  console.log(payload);
+  /*console.log(payload);*/
+  const response = await fetch(`http://localhost:5000/api/form/contact`, {
+    method: "POST",
+    body: payload,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "text/plain",
+    },
+  });
+  if (response.ok) {
+    const result = await response.json();
+    console.log(result);
+    return result;
+  }
 };
 
 export default postHandler;
